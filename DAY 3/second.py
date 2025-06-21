0a4e94b79d61f2fb1a8973c5057d620daf0d5632
MYNKCHOMIYA <MAYANKCHOMIA@GMAIL.COM>
2025-06-21 12:23:33 +0530

    first change

--- DAY 3/second.py
+++ DAY 3/second.py
@@ -1,8 +1,22 @@
-#tuples
-tup=('1','2','3','4')
-t = len(tup)
-for i in range(t):
-           print(i,tup[i])
-           print(i,tup)
-           print(i,len(tup))
+# #tuples
+# from itertools import count
 
+# tup=('1','2','3','4')
+# t = len(tup)
+# for i in range(t):
+#            print(i,tup[i])
+#            print(i,tup)
+#            print(i,len(tup))
+
+#converting list into tuples
+l =[1,2,3,4,5,6]
+tup = tuple(l)
+print(tup)
+print(type(tup))
+
+#methods of tuples
+print(f"{min(tup)}")
+print(f"{max(tup)}")
+print(f"{tup.count(3)}")
+print(f"{tup.index(3)}")
+print(f"{sum(tup)}")
---
+++ DAY 3/third.py
@@ -0,0 +1,11 @@
+#set
+s={1,2,3,4,5,"C"}
+a={"A","B","C","D","E"}
+print(s)
+print(a)
+s3 = s.union(a)
+print(s3)
+s4 = s.intersection(a)
+print(s4)
+s5= s&a
+print(s5)
